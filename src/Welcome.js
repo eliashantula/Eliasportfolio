bd2ffa8b24cb647d7bf37c4468a78cb42396cfa4
Elias Hantula <ehantula@indiana.edu>
2019-02-27 00:53:17 -0500

    upload

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Typing from 'react-typing-animation';
 import { Parallax, Background } from 'react-parallax'
 
 import { Jumbotron, Button, Container, Card, CardTitle, CardText, CardImg, CardImgOverlay} from 'reactstrap';
@@ -12,11 +12,10 @@ const Welcome = (props) => {
   <CardImgOverlay>
   <CardTitle className="display-3 welcome" style={{color:"black", margin: "0px", paddingTop: "30px",paddingBottom: "30px"}}>Welcome</CardTitle>
 <div className="restrict">
-    <CardText className="my-1 fontTest">
-   Thanks for coming by! 
-   I am an attorney turned full stack software developer with a passion for technology, learning,
-   the arts, and the law. My expertise with entities ranging from mom and pop restaurants, craft breweries to celebrities. I am always seeking out projects which promote and stimulate creativity, knowledge and enhanced life experiences. 
-  </CardText>
+    <CardText className="my-1 fontTest"><Typing speed={90}>
+   Thanks for coming by! I am an attorney turned full stack software developer with a passion for technology, learning,
+   the arts, and the law. My expertise includes working with entities ranging from mom and pop restaurants, craft breweries to celebrities. I am always seeking out projects which promote and stimulate creativity, knowledge and enhanced life experiences. 
+  </Typing></CardText>
 </div>
 </CardImgOverlay>
 </Card>
